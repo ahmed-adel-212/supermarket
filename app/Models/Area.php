3d066876c76cbef60f44874e50f299be98efd4b6
abo3adel <ahmed.adel@212solutionsllc.com>
2022-10-31 12:10:36 +0100

    create city - area relashion

--- app/Models/Area.php
+++ app/Models/Area.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Translatable\HasTranslations;
 
@@ -21,4 +22,9 @@ class Area extends Model
         'id' => 'int',
         'delivery_fees' => 'double',
     ];
+
+    public function City(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
 }

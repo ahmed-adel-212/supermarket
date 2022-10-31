72737ccaa1d9311a513ae5026f5ce0c84006f233
abo3adel <ahmed.adel@212solutionsllc.com>
2022-10-31 12:01:29 +0100

    add image to users

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -42,6 +44,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => (!empty($value) && file_exists(public_path($value))) ? url($value) : 'http://via.placeholder.com/200x200?text=No+Image',
+        );
+    }
+
     public function isAdmin()
     {
         return $this->type === 'admin';
@@ -56,4 +65,5 @@ class User extends Authenticatable
     {
         return $this->type === 'user';
     }
+    
 }

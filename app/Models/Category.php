99113f13c49c216d853c72c2604bf860667f9a84
abo3adel <ahmed.adel@212solutionsllc.com>
2022-10-31 11:33:56 +0100

    create category relationships

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,13 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Translatable\HasTranslations;
 
 class Category extends Model
 {
     use HasFactory;
-    use \Spatie\Translatable\HasTranslations;
     use SoftDeletes;
+    use HasTranslations;
 
     protected $guarded = [];
 
@@ -22,5 +25,27 @@ class Category extends Model
         return Attribute::make(
             get: fn ($value) => (!empty($value) && file_exists(public_path($value))) ? url($value) : 'http://via.placeholder.com/200x200?text=No+Image',
         );
+    }
+
+    public function is_parent(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->category_id == null,
+        );
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function categories(): HasMany
+    {
+        return $this->hasMany(Category::class, 'category_id');
+    }
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id');
     }
 }

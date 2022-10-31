16bb86eea3a3fc0b833e834a397cbcabf48f274c
abo3adel <ahmed.adel@212solutionsllc.com>
2022-10-31 12:23:47 +0100

    create banner model

---
+++ app/Models/Banner.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Banner extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    public function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => (!empty($value) && file_exists(public_path($value))) ? url($value) : 'http://via.placeholder.com/200x200?text=No+Image',
+        );
+    }
+
+}

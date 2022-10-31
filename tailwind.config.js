77bcbf3ac9045d43904b4c0eee3cbef7d072f50f
abo3adel <ahmed.adel@212solutionsllc.com>
2022-10-31 14:17:28 +0100

    configure vite and assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    module: "jit",
+
+    darkMode: "class",
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
